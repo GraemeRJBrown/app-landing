bb8dad4efafd3109c3e60a0e9614b86fd7c80ed9
Graeme Brown <graemerjbrown@googlemail.com>
2020-04-02 21:56:35 +0100

    Application v0.1.0

---
+++ precache-manifest.b06095fb9e55e500a27d79f165d5ba6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dbaf222b39342449dc61d67109236e",
+    "url": "/app-landing/index.html"
+  },
+  {
+    "revision": "13bcaeb31be901cae84b",
+    "url": "/app-landing/static/css/2.aaca92c8.chunk.css"
+  },
+  {
+    "revision": "6a239721ccc35b198fb9",
+    "url": "/app-landing/static/css/main.7b2833bc.chunk.css"
+  },
+  {
+    "revision": "13bcaeb31be901cae84b",
+    "url": "/app-landing/static/js/2.0da24c3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app-landing/static/js/2.0da24c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a239721ccc35b198fb9",
+    "url": "/app-landing/static/js/main.79caaf4c.chunk.js"
+  },
+  {
+    "revision": "cdad876cd1a400294778",
+    "url": "/app-landing/static/js/runtime-main.2b717d27.js"
+  }
+]);
